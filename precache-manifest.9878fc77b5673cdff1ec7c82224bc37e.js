127870b3218aa2d3606862276ad8c609b92e6f8a
Abdul <arkaimkhani06@gmail.com>
2020-04-07 16:00:40 +0500

    Updates

---
+++ precache-manifest.9878fc77b5673cdff1ec7c82224bc37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eda1baf9adcf9cfc9ea59b103af0b33",
+    "url": "/Reminder_Pro/index.html"
+  },
+  {
+    "revision": "f850aa048338d1fca310",
+    "url": "/Reminder_Pro/static/css/main.e6df2eef.chunk.css"
+  },
+  {
+    "revision": "4dc8db24c62092ee8869",
+    "url": "/Reminder_Pro/static/js/2.51879d47.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Reminder_Pro/static/js/2.51879d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f850aa048338d1fca310",
+    "url": "/Reminder_Pro/static/js/main.069f7e9c.chunk.js"
+  },
+  {
+    "revision": "4e8619c727a70ecc19e6",
+    "url": "/Reminder_Pro/static/js/runtime-main.8f1d3b46.js"
+  }
+]);
